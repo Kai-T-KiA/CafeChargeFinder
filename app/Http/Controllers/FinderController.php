63e58abf83c877d510f8437514f231ec6c4164a1
Kai-T-KiA <24vr026m@rikkyo.ac.jp>
2024-08-24 14:19:23 +0900

    ユーザー入力による新店舗登録機能の実装

--- app/Http/Controllers/FinderController.php
+++ app/Http/Controllers/FinderController.php
@@ -7,6 +7,9 @@ use App\Models\Place;
 use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use GoogleMaps;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
 
 class FinderController extends Controller
 {
@@ -30,11 +33,53 @@ class FinderController extends Controller
     public function store(Request $request, Place $place, Post $post) {
         
         // ユーザーが入力した店舗名
-        $user_input_place_name = $request['place'];
+        $user_input_place_name = $request['place']['name'];
+        // $user_input_place_name = 'サンマルクカフェ 目白駅前店';
+        
+        // dump($user_input_place_name);
+        
+        // ------------------------------------------
+        
+        // $query = $user_input_place_name.' 営業時間';
+        // $apiKey = env('GOOGLE_MAP_API');
+        // $cx = env('GOOGLE_CSE_ID');
+    
+        // $response = Http::get('https://www.googleapis.com/customsearch/v1', [
+        //     'key' => $apiKey,
+        //     'cx' => $cx,
+        //     'q' => $query,
+        // ]);
+    
+        // $results = $response->json();
+        
+        // dump($results);
+    
+        // return view('search_results', ['results' => $results['items']]);
+        // ーーーーーーーーーーーーーーーーーーー
+        
+        // $response = Http::get('https://maps.googleapis.com/maps/api/place/textsearch/json', [
+        //     'query' => $query,
+        //     'key' => $apiKey,
+        //     'language' => 'ja',
+        // ]);
+    
+        // $placeDetails = $response->json();
+        
+        // dump($placeDetails);
+        
+        // ーー-----------------
+        
+        
         
         // Google Maps APIで店舗名を統一
         $googlemap_data = $this->get_place_detail_by_googlemap($user_input_place_name);
         $regist_name = $googlemap_data['name'];
+        
+        // dump($regist_name);
+        // dd($googlemap_data);
+        
+        // dump($googlemap_data['opening_hours']);
+        // dd(json_encode($googlemap_data['opening_hours'], JSON_UNESCAPED_UNICODE));
         
         // 既に店舗データが登録されているか確認
         $place = Place::where('name', $regist_name)->first();
@@ -44,7 +89,7 @@ class FinderController extends Controller
             $place = new Place();
             $place->name = $regist_name;
             $place->address = $googlemap_data['address'];
-            $place->opening_hours = $googlemap_data['opening_hours'];
+            $place->opening_hours = json_encode($googlemap_data['opening_hours'], JSON_UNESCAPED_UNICODE);
             $place->latitude = $googlemap_data['latitude'];
             $place->longitude = $googlemap_data['longitude'];
             $place->save();
@@ -58,25 +103,57 @@ class FinderController extends Controller
         $post->save();
         
         
-        return redirect('/')->with('message', '店舗データが正常に登録されました。');
+        return redirect('/finder/home')->with('message', '店舗データが正常に登録されました。');
         // return redirect()->route('finder.regist')->with('message', '店舗データが正常に登録されました。');
     }
     
     private function get_place_detail_by_googlemap($user_input_place_name) {
         // Googel Maps APIで店舗情報を取得
-        $response = GoogleMaps::load('place/textsearch')->setParam([
-            'query' => $user_input_place_name,
-            'key' => env('GOOGLE_MAPS_API_KEY'),
-        ])->get();
+        // $response = \GoogleMaps::load('place/textsearch')->setParam([
+        // $response = \GoogleMaps::load('textsearch')->setParam([
+        //     'query' => $user_input_place_name,
+        //     'key' => env('GOOGLE_MAP_API'),
+        //     'region' => 'jp',
+        //     'language' => 'ja',
+        // ])->get();
         
-        $place_data = json_decode($response, true)['results'][0];
+        $response = Http::get('https://maps.googleapis.com/maps/api/place/textsearch/json', [
+            'query' => $user_input_place_name,
+            'key' => env('GOOGLE_MAP_API'),
+            'language' => 'ja',
+        ]);
+    
+        $place_data = $response->json();
+        
+        
+        $placeId = $place_data['results'][0]['place_id'];
+
+        // 2. Place Details APIを使って営業時間を取得する
+        $detailsResponse = Http::get('https://maps.googleapis.com/maps/api/place/details/json', [
+            'place_id' => $placeId,
+            'fields' => 'opening_hours',
+            'key' => env('GOOGLE_MAP_API'),
+            'language' => 'ja',
+        ]);
+
+        $details = $detailsResponse->json();
+        
+        // dump($response);
+        
+        
+        // // $place_data = json_decode($response, true)['results'];
+        // $place_data = json_decode($response, true);
+        
+        // dump($place_data);
+        
+        // dump($details);
         
         return [
-            'name' => $place_data['name'],
-            'address' => $place_data['formatted_address'],
-            'opening_hours' => $place_data['opening_hours']['weekday_text'] ?? null,
-            'latitude' => $place_data['geometry']['location']['lat'],
-            'longitude' => $place_data['geometry']['location']['lng'],
+            'name' => $place_data['results'][0]['name'],
+            'address' => $place_data['results'][0]['formatted_address'],
+            'opening_hours' => $details['result']['opening_hours']['weekday_text'],
+            'latitude' => $place_data['results'][0]['geometry']['location']['lat'],
+            'longitude' => $place_data['results'][0]['geometry']['location']['lng'],
         ];
     }
     
--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         // GoogleMaps\ServiceProviders\GoogleMapsServiceProvider::class,
+        GoogleMaps\ServiceProvider\GoogleMapsServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,6 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         // 'GoogleMaps' => Alexpechkarev\GoogleMaps\Facades\GoogleMapsFacades::class,
+        'GoogleMaps' => GoogleMaps\Facade\GoogleMapsFacade::class,
     ])->toArray(),
 
 ];
